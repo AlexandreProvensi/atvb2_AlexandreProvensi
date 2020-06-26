b64dab532d2b614af05f330901b94bd711d2bceb
AlexandreProvensi <alexandreprovensi12@gmail.com>
2020-06-26 00:40:08 -0300

    Trabalho ATVB2 - 80%

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 appATVB2 = angular.module('appATVB2', ['ngRoute', 'ngStorage']);
 
+//Configurar as rotas da aplicação
 appATVB2.config(function($routeProvider) {
     $routeProvider
         .when('/', {
@@ -14,5 +15,30 @@ appATVB2.config(function($routeProvider) {
             templateUrl: 'app/template/admin/produto.view.html',
             controller: 'ProdutoController'
         })
+        .when('/signin', {
+            templateUrl: 'app/template/admin/signin.view.html',
+            controller: 'SignController'
+        })
+        .when('/signup', {
+            templateUrl: 'app/template/admin/signup.view.html',
+            controller: 'SignController'
+        })
+        .when('/contato', {
+            templateUrl: 'app/template/contato.view.html'
+        })
         .otherwise({ redirectTo: '/' });
-})
+});
+
+//Defino o que acontecerá na execução da aplicação
+appATVB2.run(function($rootScope, $location, $sessionStorage) {
+
+    $rootScope.$on('$locationChangeStart', function() {
+
+        if ($location.path().indexOf('sign') < 0) {
+            //Verifica se o usuário entrou
+            if (!$sessionStorage.logado) {
+                $location.path('/signin');
+            }
+        }
+    })
+});
